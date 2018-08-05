a0c68a0d30d8891fdff2ab739ab01a4d379aba1d
Spencer Shadley <spencershadley@utexas.edu>
2018-08-05 14:59:36 -0700

    setup reducer

---
+++ src/reducers/index.test.tsx
@@ -0,0 +1,17 @@
+import {enthusiasm} from "./index";
+import {decrementEnthusiasm, incrementEnthusiasm} from "../actions/index";
+
+it('should increment enthusiasm', () => {
+    const updatedState = enthusiasm({languageName: '', enthusiasmLevel: 0}, incrementEnthusiasm() );
+    expect(updatedState.enthusiasmLevel).toEqual(1);
+});
+
+it('should decrement enthusiasm', () => {
+    const updatedState = enthusiasm({languageName: '', enthusiasmLevel: 5}, decrementEnthusiasm() );
+    expect(updatedState.enthusiasmLevel).toEqual(4);
+});
+
+it('should not decrement below 1', () => {
+    const updatedState = enthusiasm({languageName: '', enthusiasmLevel: 1}, decrementEnthusiasm() );
+    expect(updatedState.enthusiasmLevel).toEqual(1);
+});
