bce6062cf665d11926c76769bb8fa3bd4cd62df1
Spencer Shadley <spencershadley@utexas.edu>
2018-08-04 20:44:46 -0700

    more tests

--- src/components/Hello.test.tsx
+++ src/components/Hello.test.tsx
@@ -6,3 +6,19 @@ it('renders the correct text when no enthusiasm level is given', () => {
    const hello = enzyme.shallow(<Hello name='Spencer' />);
    expect(hello.find('.greeting').text()).toEqual('Hello Spencer!');
 });
+
+it('renders the correct text with an explicit enthusiasm of 1', () => {
+    const hello = enzyme.shallow(<Hello name='Spencer' enthusiasmLevel={1}/>);
+    expect(hello.find(".greeting").text()).toEqual('Hello Spencer!')
+});
+
+it('renders the correct text with an explicit enthusiasm level of 5', () => {
+    const hello = enzyme.shallow(<Hello name='Spencer' enthusiasmLevel={5} />);
+    expect(hello.find(".greeting").text()).toEqual('Hello Spencer!!!!!');
+});
+
+it('throws when the enthusiasm level is negative', () => {
+    expect(() => {
+        enzyme.shallow(<Hello name='Spencer' enthusiasmLevel={-1} />);
+    }).toThrow();
+});
