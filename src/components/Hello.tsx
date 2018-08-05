b775ed0f95ee6dc3d199719151433d45ab765651
Spencer Shadley <spencershadley@utexas.edu>
2018-08-04 19:59:01 -0700

    added buttons to change enthusiasm

--- src/components/Hello.tsx
+++ src/components/Hello.tsx
@@ -5,18 +5,39 @@ export interface IHelloProps {
     enthusiasmLevel?: number;
 }
 
-function Hello({ name, enthusiasmLevel = 1 }: IHelloProps) {
-    if (enthusiasmLevel <= 0) {
-        throw new Error('Whomp whomp...')
+interface IHelloState {
+    currentEnthusiasm: number;
+}
+
+class Hello extends React.Component<IHelloProps, IHelloState> {
+    constructor(props: IHelloProps) {
+        super(props);
+        this.state = { currentEnthusiasm: props.enthusiasmLevel || 1 };
     }
 
-    return (
-        <div className="hello">
-            <h1 className="greeting">
-                Hello {name + getExclamationMarks(enthusiasmLevel)}
-            </h1>
-        </div>
-    );
+    public render() {
+        const { name } = this.props;
+
+        if(this.state.currentEnthusiasm <= 0) {
+            throw new Error('Whomp whomp...');
+        }
+        return (
+            <div className="hello">
+                <h1 className="greeting">
+                    Hello {name + getExclamationMarks(this.state.currentEnthusiasm)}
+                </h1>
+                <button onClick={this.onDecrement}>-</button>
+                <button onClick={this.onIncrement}>+</button>
+            </div>
+        );
+    }
+
+    private onIncrement = () => this.updateEnthusiasm(this.state.currentEnthusiasm + 1);
+    private onDecrement = () => this.updateEnthusiasm(this.state.currentEnthusiasm - 1);
+
+    private updateEnthusiasm(currentEnthusiasm: number) {
+        this.setState({currentEnthusiasm});
+    }
 }
 
 export default Hello;
